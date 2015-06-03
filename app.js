3fa08843e4240e0782f4b9793014caddcad89e5c
Tara Gee <geetar40@gmail.com>
2015-06-03 11:24:42 -0600

    attemtps

---
+++ app.js
@@ -0,0 +1,6 @@
+var foo = require('./lib.js');
+
+foo.add(1, 2);
+foo.sub(5, 3);
+foo.multi(4, 5);
+foo.div(6, 12);
---
+++ lib.js
@@ -0,0 +1,14 @@
+module.exports = {
+    add: function(num1, num2){
+      console.log(num1 + num2);
+    },
+    sub: function(num1, num2){
+      console.log(num1 - num2);
+    },
+    multi:function(num1, num2){
+      console.log(num1 * num2);
+    },
+    div: function(num1, num2){
+      console.log(num2/num1);
+    }
+}
